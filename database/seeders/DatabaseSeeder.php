fbf1e6f9121aa72da710a616a4ed275f67d01449
Valeria Stamenova <stam0012@hz.nl>
2023-06-06 09:52:52 +0200

    Remove unused stuff from the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User'
         //     'email' => 'test@example.com',
