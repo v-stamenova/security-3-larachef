3443c4d92e6d1b688187c425ed35866d5482c245
Valeria Stamenova <stam0012@hz.nl>
2023-06-06 10:47:48 +0200

    Alter composer.json with faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(10)->create();
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User'
         //     'email' => 'test@example.com',
